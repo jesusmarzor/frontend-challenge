2cc2c5a5813d250a5f299740e66e0ead805a2579
Jesus Martin <jesusmarzor28@gmail.com>
2022-05-29 19:28:41 +0200

    Update Products Table

--- src/partials/products/ProductsTable.jsx
+++ src/partials/products/ProductsTable.jsx
@@ -1,123 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Products from './ProductsTableItem';
+import { useTranslation } from 'react-i18next';
+import { useProducts } from '../../hooks/useProducts';
+import { VariablesConsumer } from '../../contexts/variablesContext';
+import { Pagination } from '../../components/Pagination';
+import { ACTIONS_VARIABLES, HEADER_PRODUCTS, ORDERS } from '../../utils/Constants';
 
 function ProductsTable() {
-
-  const products = [
-    {
-      id: '0',
-      product: '#123567',
-      total: '$129.00',
-      status: 'Overdue',
-      customer: 'Dominik Lamakani',
-      issueddate: '22/07/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    },
-    {
-      id: '1',
-      product: '#779912',
-      total: '$59.00',
-      status: 'Paid',
-      customer: 'Mark Cameron',
-      issueddate: '19/07/2021',
-      paiddate: '20/07/2021',
-      type: 'Subscription',
-    },
-    {
-      id: '2',
-      product: '#889924',
-      total: '$89.00',
-      status: 'Paid',
-      customer: 'Sergio Gonnelli',
-      issueddate: '17/07/2021',
-      paiddate: '19/07/2021',
-      type: 'One-time',
-    },
-    {
-      id: '3',
-      product: '#897726',
-      total: '$129.00',
-      status: 'Due',
-      customer: 'Manuel Garbaya',
-      issueddate: '04/07/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    },
-    {
-      id: '4',
-      product: '#123567',
-      total: '$129.00',
-      status: 'Due',
-      customer: 'Cool Robot',
-      issueddate: '04/07/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    },
-    {
-      id: '5',
-      product: '#896644',
-      total: '$129.00',
-      status: 'Paid',
-      customer: 'Mark Cameron',
-      issueddate: '04/07/2021',
-      paiddate: '09/07/2021',
-      type: 'One-time',
-    },
-    {
-      id: '6',
-      product: '#136988',
-      total: '$69.00',
-      status: 'Paid',
-      customer: 'Glenn Thomas',
-      issueddate: '01/07/2021',
-      paiddate: '01/07/2021',
-      type: 'One-time',
-    },
-    {
-      id: '7',
-      product: '#442206',
-      total: '$129.00',
-      status: 'Overdue',
-      customer: 'Dominik Lamakani',
-      issueddate: '22/06/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    },
-    {
-      id: '8',
-      product: '#764321',
-      total: '$89.00',
-      status: 'Paid',
-      customer: 'Brian Halligan',
-      issueddate: '21/06/2021',
-      paiddate: '29/06/2021',
-      type: 'One-time',
-    },
-    {
-      id: '9',
-      product: '#908764',
-      total: '$129.00',
-      status: 'Due',
-      customer: 'Carolyn McNeail',
-      issueddate: '17/06/2021',
-      paiddate: '-',
-      type: 'Subscription',
-    }
-  ];
-
-  const [list, setList] = useState([]);
-
-  useEffect(() => {
-    setList(products);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-
+  const { t } = useTranslation();
+  const {variables, setVariables} = VariablesConsumer();
+  const {products, pagination, loading} = useProducts({variables});
+  const changeOrder = (option) => {
+    (variables.order_by === option)
+    ?
+      (variables.order === ORDERS.ASC)
+      ?
+        setVariables({type: ACTIONS_VARIABLES.UPDATE_ORDER, payload: ORDERS.DESC})
+      :
+        setVariables({type: ACTIONS_VARIABLES.UPDATE_ORDER, payload: ORDERS.ASC})
+    :
+      setVariables({type: ACTIONS_VARIABLES.UPDATE_ORDER_BY, payload: option})
+  }
   return (
+    <>
     <div className="bg-white shadow-lg rounded-sm border border-slate-200 relative">
       <header className="px-5 py-4">
-        <h2 className="font-semibold text-slate-800">Productos <span className="text-slate-400 font-medium">67</span></h2>
+        <h2 className="font-semibold text-slate-800">{t('Products')} <span className="text-slate-400 font-medium">{pagination.totalResults}</span></h2>
       </header>
       <div>
 
@@ -127,54 +35,69 @@ function ProductsTable() {
             {/* Table header */}
             <thead className="text-xs font-semibold uppercase text-slate-500 bg-slate-50 border-t border-b border-slate-200">
               <tr>
-                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Producto</div>
-                </th>
-                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Total</div>
-                </th>
-                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Estado</div>
-                </th>
-                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Cliente</div>
-                </th>
-                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Emitido el</div>
-                </th>
-                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Pagado el</div>
-                </th>
-                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="font-semibold text-left">Tipo</div>
-                </th>
+                {
+                  HEADER_PRODUCTS.map( option => {
+                    return (
+                      <th key={option} className="relative text-left px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                        {
+                        <button onClick={() => changeOrder(option)} className="w-full font-semibold text-left uppercase">
+                          {t(option)}
+                          {
+                          (variables.order_by === option)
+                          &&
+                          <svg className={`ml-1 absolute right-2 top-0 bottom-0 my-auto ${(variables.order === ORDERS.ASC) && "rotate-180"}`} width="12" height="12" viewBox="0 0 129 129">
+                            <g>
+                              <path className="fill-gray-500" d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z"/>
+                            </g>
+                          </svg>
+                          }
+                        </button>
+                        }
+                      </th>
+                    )
+                  })
+                }
               </tr>
             </thead>
             {/* Table body */}
             <tbody className="text-sm divide-y divide-slate-200">
-              {
-                list.map(product => {
-                  return (
-                    <Products
-                      key={product.id}
-                      id={product.id}
-                      product={product.product}
-                      total={product.total}
-                      status={product.status}
-                      customer={product.customer}
-                      issueddate={product.issueddate}
-                      paiddate={product.paiddate}
-                      type={product.type}
-                    />
-                  )
-                })
-              }
-            </tbody>
+            {
+              (!loading)
+              &&
+              products.map(product => {
+                return (
+                  <Products
+                    key={product.id}
+                    id={product.id}
+                    title={product.title}
+                    price={product.price}
+                    stock={product.stock}
+                    tax={product.tax}
+                  />
+                )
+              })
+            }
+          </tbody>
           </table>
 
+          {
+          (loading) 
+          ? 
+            <p className="w-full text-center my-5">{t('Loading')}</p>
+          :
+            (products.length === 0) && <p className="w-full text-center my-5">{t('Products_dont_exist')}</p>
+          }
         </div>
       </div>
     </div>
+    {
+      (products.length !== 0)
+      &&
+      <div className="mt-8">
+        <Pagination pagination={pagination}/>
+      </div>
+    }
+  </>
   );
 }
 
