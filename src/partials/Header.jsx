5ef8d72656e5c76e7eb2885094670283689b44da
Jesus Martin <jesusmarzor28@gmail.com>
2022-05-29 19:26:46 +0200

    Add flags to change the language

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import spain from './../assets/img/spain.png';
+import england from './../assets/img/england.png';
+import { useTranslation } from "react-i18next";
+import { LANGUAGES } from '../utils/Constants';
 
 function Header() {
+  const { t, i18n } = useTranslation();
 
+  const changeLanguage = (lang) => {
+    if(i18n.language !== lang){
+      i18n.changeLanguage(lang);
+      localStorage.setItem('lang', lang);
+    } 
+  }
 
   return (
     <header className="sticky top-0 bg-white border-b border-slate-200 z-30">
@@ -14,8 +25,9 @@ function Header() {
 
           {/* Header: Right side */}
           <div className="flex items-center space-x-3">
-            {/* Suggested space for translation flags */}...
-
+            {/* Suggested space for translation flags */}
+            <img onClick={() => changeLanguage(LANGUAGES.EN)} className={`cursor-pointer transform duration-300 ease-in-out${(i18n.language === LANGUAGES.EN) && 'transform scale-[1.2]'}`} width={40} src={england} alt={t('England_flag')}/>
+            <img onClick={() => changeLanguage(LANGUAGES.ES)} className={`cursor-pointer transform duration-300 ease-in-out${(i18n.language === LANGUAGES.ES) && 'transform scale-[1.2]'}`} width={40} src={spain} alt={t('Spain_flag')}/>
           </div>
 
         </div>
